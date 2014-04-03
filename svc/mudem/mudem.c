635b5ee33b08bd5c6770b4f136d002f160680814
morpheus42 <morpheus42@gmail.com>
2014-04-04 01:51:46 +0200

    Playing with mudem

--- svc/mudem/mudem.c
+++ svc/mudem/mudem.c
@@ -1,7 +1,7 @@
 
 
 
-
+#include "stddef.h"
 #include "mudem.h"
 
 
@@ -12,10 +12,18 @@
 
 typedef struct
 {
-  uint8_t channel:3;
-  uint8_t esc:1;
-  uint8_t pos:1;
-  uint8_t valid:1;
+  uint8_t chIn:3;
+  uint8_t escIn:1;
+  uint8_t posIn:1;
+  uint8_t validIn:1;
+  uint8_t stuff0:2;
+  
+  uint8_t chOut:3;
+  uint8_t escOut:1;
+  uint8_t posOut:2;
+
+  uint8_t cntOut;
+  uint8_t cOut;
 }mudem_inst;
 
 
@@ -23,24 +31,24 @@ static mudem_inst this;
 
 void CallRecv(uint8_t b)
 {
-  if (this.valid)
+  if (this.validIn)
   {
-    ByteReceived_t * f = mudem_channels[this.channel].pfnByteReceived;
+    ByteReceived_t * f = mudem_channels[this.chIn].pfnByteReceived;
     if (f)
     {
-      f(b);
+      f(b | MUDEM_DATA);
     }
   }
 }
 
 void CallCtrl(uint8_t b)
 {
-  if (this.valid)
+  if (this.validIn)
   {
-    ByteReceived_t * f = mudem_channels[this.channel].pfnControl;
+    ByteReceived_t * f = mudem_channels[this.chIn].pfnByteReceived;
     if (f)
     {
-      f(b);
+      f(b | MUDEM_CTRL);
     }
   }
 }
@@ -49,31 +57,31 @@ void mudem_ByteReceived(uint8_t b)
 {
   if (b==MUDEM_START)
   {
-    this.pos=0;
-    this.esc=0;
+    this.posIn=0;
+    this.escIn=0;
   }
   else
   {
     if (b==MUDEM_ESC)
     {
-      this.esc=1;
+      this.escIn=1;
     }
     else
     {
-      if (this.esc==1)
+      if (this.escIn==1)
       {
         b ^= MUDEM_ESC_OFS;
-        this.esc=0;
+        this.escIn=0;
       }
-      if (this.pos==0)
+      if (this.posIn==0)
       {
-        this.channel = b >> 5;
-        if (this.channel<=sizeof(mudem_channels)/sizeof(mudem_channel_t))
+        this.chIn = b >> 5;
+        if (this.chIn<=sizeof(mudem_channels)/sizeof(mudem_channel_t))
         {
-          this.valid=1;
+          this.validIn=1;
         }
         CallCtrl(b);
-        this.pos=1;
+        this.posIn=1;
       }
       else
       {
@@ -81,11 +89,103 @@ void mudem_ByteReceived(uint8_t b)
       }
     }
   
-  
   }
 
 }
 
+
+
+uint16_t mudem_GetOut(void)
+{
+  ByteTransmit_t * pfnGetOut;
+  uint8_t c,ch;
+  uint16_t w;
+
+  if (this.escOut)
+  { // previous char was escape char
+    this.escOut=0;
+    return this.cOut | 0x100;
+  }
+
+  if (this.posOut==0)
+  { // previous char was a start, now send channel
+    uint8_t c = this.chOut<<5;
+    this.posOut=1;
+    return c | 0x100;
+  }
+    
+  if (this.posOut==1)
+  { // previous char was channel, now send data
+    this.posOut=2;
+    return this.cOut | 0x100;
+  }
+  
+  ch=this.chOut;
+  #if 0
+  if (this.cntOut>63)
+  {
+    ch++;
+    if (ch>=sizeof(mudem_channels)/sizeof(mudem_channel_t))
+    {
+      ch=0;
+    }
+    this.cntOut=0;
+  }
+  #endif
+    
+  while((pfnGetOut = mudem_channels[ch].pfnByteTransmit)==NULL)
+  {
+    ch++;
+    if (ch>=sizeof(mudem_channels)/sizeof(mudem_channel_t))
+    {
+      ch=0;
+    }
+    this.cntOut=0;
+  }
+    
+  w = pfnGetOut();
+  if (w)
+  { // we got new data to send
+    uint8_t c = w;
+    if ((c==MUDEM_START) || (c==MUDEM_ESC))
+    {
+      this.escOut=1;
+      c ^= MUDEM_ESC_OFS;
+    }
+    this.cOut = c;
+  
+  
+    if (ch!=this.chOut)
+    { // different channel!!
+      this.posOut=0;
+      this.escOut=0;
+    }
+    
+
+  }
+  else
+  {
+    ch++;
+    if (ch>=sizeof(mudem_channels)/sizeof(mudem_channel_t))
+    {
+      ch=0;
+    }
+    this.chOut=ch;
+    return 0;
+  }
+  this.chOut=ch;
+
+
+  if (this.posOut==0)
+  {
+    return  MUDEM_START | 0x100;
+  }
+  if (this.escOut)
+  {
+    return MUDEM_ESC |0x100;
+  }
+  return this.cOut | 0x100;
+}
 
 
 
--- svc/mudem/mudem.h
+++ svc/mudem/mudem.h
@@ -5,17 +5,30 @@
 
 #include "stdint.h"
 
-typedef void ByteReceived_t(uint8_t b);
+
+#define MUDEM_NODATA     0x000
+#define MUDEM_DATA       0x100
+#define MUDEM_CTRL       0x200
+
+#define MUDEM_CTRL_START    0x01
+#define MUDEM_CTRL_CONTINUE 0x02
+
+
+
+typedef void ByteReceived_t(uint16_t b);
+typedef uint16_t ByteTransmit_t(void);
 
 typedef struct
 {
   ByteReceived_t * pfnByteReceived;
-  ByteReceived_t * pfnControl;
+  ByteTransmit_t * pfnByteTransmit;
 }mudem_channel_t;
 
 extern mudem_channel_t mudem_channels[8];
 
 void mudem_ByteReceived(uint8_t b);
+
+uint16_t mudem_GetOut(void);
 
 
 
