bc5cedf03ba3aca36c25a1480b828209df307bf9
morpheus42 <morpheus42@gmail.com>
2014-10-02 01:35:48 +0200

    Added files for beginning of a very simple events module.

---
+++ util/evts/evts.h
@@ -0,0 +1,26 @@
+
+
+#ifndef _EVTS_H_
+#define _EVTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stdint.h"
+
+typedef void evtsfun_t( void );
+
+
+
+
+
+
+
+#ifdef __cplusplus
+} //extern C
+#endif
+
+
+#endif //_EVTS_H_
+
